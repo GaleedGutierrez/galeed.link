28e1bcc463faef07d020dbe4cd5d7353d9f691a1
galeed gutierrez <galeedeveloper@gmail.com>
2025-07-27 22:33:42 -0300

    feat: configure Astro with sitemap, icon, and Sonda integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
+/* eslint-disable new-cap */
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+import Sonda from 'sonda/astro';
+
+process.loadEnvFile();
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://galeed.link',
+	vite: {
+		build: {
+			sourcemap: process.env.ENABLE_SOURCEMAP === 'true',
+		},
+	},
 	server: {
 		host: true,
 	},
+	integrations: [
+		sitemap(),
+		icon(),
+		process.env.ENABLE_SOURCEMAP === 'true' && Sonda({ server: true }),
+	],
 });
