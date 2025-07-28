2fff79bc6f8137c64ca40225f820ee2ec2120df5
galeed gutierrez <galeedeveloper@gmail.com>
2025-07-28 13:23:45 -0300

    feat: enhance Astro configuration with Partytown and Playform compression integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 /* eslint-disable new-cap */
 // @ts-check
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+import playformCompress from '@playform/compress';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import Sonda from 'sonda/astro';
@@ -14,6 +16,11 @@ export default defineConfig({
 		build: {
 			sourcemap: process.env.ENABLE_SOURCEMAP === 'true',
 		},
+		define: {
+			'import.meta.env.GOOGLE_ANALYTICS_ID': JSON.stringify(
+				process.env.GOOGLE_ANALYTICS_ID,
+			),
+		},
 	},
 	server: {
 		host: true,
@@ -21,6 +28,22 @@ export default defineConfig({
 	integrations: [
 		sitemap(),
 		icon(),
-		process.env.ENABLE_SOURCEMAP === 'true' && Sonda({ server: true }),
+		process.env.ENABLE_SOURCEMAP === 'true' &&
+			Sonda({ server: true, open: false }),
+		partytown({
+			config: {
+				forward: ['dataLayer.push', 'gtag'],
+			},
+		}),
+		playformCompress({
+			CSS: {
+				csso: false,
+			},
+			HTML: {
+				'html-minifier-terser': {
+					removeComments: true,
+				},
+			},
+		}),
 	],
 });
