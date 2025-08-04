b76c27fa9db646f2bc9c800b39caed5fac949e8f
galeed gutierrez <galeedeveloper@gmail.com>
2025-08-04 16:31:48 -0300

    feat: update cssnano preset to 'advanced' and remove postcss-nested plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 import cssnano from 'cssnano';
 import postcssCustomProperties from 'postcss-custom-properties';
-import postcssNested from 'postcss-nested';
 import postcssUtopia from 'postcss-utopia';
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: [
-		cssnano({ preset: ['default'] }),
-		postcssNested,
 		postcssCustomProperties({ preserve: false }),
 		postcssUtopia({ minWidth: 360, maxWidth: 448 }),
+		cssnano({
+			preset: ['advanced', {}],
+		}),
 	],
 };
 
