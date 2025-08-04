cef2962c63526067506a0fc2189a7da227148795
galeed gutierrez <galeedeveloper@gmail.com>
2025-08-04 16:48:13 -0300

    feat: add postcss-mixins to package.json and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 import cssnano from 'cssnano';
 import postcssCustomProperties from 'postcss-custom-properties';
+import postcssMixins from 'postcss-mixins';
 import postcssUtopia from 'postcss-utopia';
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: [
+		postcssMixins,
 		postcssCustomProperties({ preserve: false }),
 		postcssUtopia({ minWidth: 360, maxWidth: 448 }),
 		cssnano({
